f65dbf31e0090adcd9f48c6bc34c7aa2d424c6e2
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-13 02:33:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Susan"
 var height = "70"
-var correct height = 70
-var correct = parseInt(correct height)
+var correct height = 70;
+var correct = parseInt(correct height);
 // Don't worry about this
 module.exports = { name, height
 }
